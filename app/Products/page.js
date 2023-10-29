59e393f474f568db49474b8d6a5a337aae36eada
RAHIL PRANSHUTA TOPNO <57543411+Rahil32@users.noreply.github.com>
2023-10-30 00:05:55 +0530

    fix

--- app/Products/page.js
+++ app/Products/page.js
@@ -1,26 +1,27 @@
-"use client"
-import React,{useEffect,useState} from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
 
 const Page = () => {
-    const [data, setdata] = useState([])
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const result = await fetch("https://dummyjson.com/products");
-            if (!result.ok) {
-              throw new Error('Network response was not ok');
-            }
-            const data = await result.json();
-            console.log(data.products);
-            setdata(data.products);
-          } catch (error) {
-            console.error('Error fetching data:', error);
-          }
-        };
-      
-        fetchData();
-      }, []);
-      
+  const [data, setdata] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await fetch("https://dummyjson.com/products");
+        if (!result.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await result.json();
+        console.log(data.products);
+        setdata(data.products);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="mx-auto grid w-full max-w-7xl items-center space-y-4 px-2 py-10 md:grid-cols-2 md:gap-6 md:space-y-0 lg:grid-cols-4">
       {data.map((items, i) => (
@@ -34,9 +35,7 @@ const Page = () => {
             <h1 className="inline-flex items-center text-lg font-semibold">
               {items.title}
             </h1>
-            <p className="mt-3 text-sm text-gray-600">
-              {items.description}
-            </p>
+            <p className="mt-3 text-sm text-gray-600">{items.description}</p>
             <div className="mt-4">
               <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
                 {items.category}
@@ -48,12 +47,13 @@ const Page = () => {
                 {items.price}
               </span>
             </div>
-            <button
+            <Link
+              href={`/product/${items.id}`}
               type="button"
               className="mt-4 w-full rounded-sm bg-black px-2 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
             >
-              Add to Cart
-            </button>
+              Details
+            </Link>
           </div>
         </div>
       ))}
